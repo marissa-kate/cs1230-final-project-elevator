59932cae6f2ce5f6c7d6521c67adfa82f850e1f6
marissa-kate <rissa6204@gmail.com>
2025-12-07 13:16:07 -0500

    marissa code happy

---
+++ build/build-projects_realtime-Qt_6_9_2_for_macOS_qt_qt6-Debug/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/marissashaffer/Desktop/cs/cs1230/cs1230-final-project-elevator/build/build-projects_realtime-Qt_6_9_2_for_macOS_qt_qt6-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/marissashaffer/Qt/6.9.2/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/marissashaffer/Qt/6.9.2/macos" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "/Users/marissashaffer/Qt/MaintenanceTool.app/Contents/MacOS/MaintenanceTool" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
